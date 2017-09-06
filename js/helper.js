e6f8ea5aa167a5ab8662e69502946a7f7d7d6899
Tiago Bember Simeao <tbembersimeao@ufl.edu>
2017-09-06 10:13:08 -0400

    Little adjustment on helper.js.

--- js/helper.js
+++ js/helper.js
@@ -24,29 +24,28 @@ $(document).ready(function() {
 
         // Setting up action tags description texts.
         var texts = {
-            '@DEFAULT-FROM-FIELD': 'Sets a fields\'s default value from an existing field on the same form. This is useful when using hidden fields as source for visible fields, e.g. @DEFAULT-FROM-FIELD=\'hidden_first_name\'.',
+            '@DEFAULT-FROM-PREVIOUS-EVENT': 'Sets a field\'s default value based on its own value in a previous event. To map the default value from another field, you may specify the source as a parameter to the action tag, e.g @DEFAULT-FROM-PREVIOUS-EVENT="source_field".',
             '@DEFAULT-WHEN-VISIBLE': 'If the field is visible it sets the initial value otherwise it removes the value. This is mainly useful in fields which are visible and hidden by branching logic, e.g. @DEFAULT-FROM-FIELD=\'10\'.',
-            '@DEFAULT-FROM-PREVIOUS-EVENT': 'Sets a field\'s default value based on its own value in a previous event. To map the default value from another field, you may specify the source as a parameter to the action tag, e.g @DEFAULT-FROM-PREVIOUS-EVENT="source_field".'
+            '@DEFAULT-FROM-FIELD': 'Sets a fields\'s default value from an existing field on the same form. This is useful when using hidden fields as source for visible fields, e.g. @DEFAULT-FROM-FIELD=\'hidden_first_name\'.'
         };
 
-        var $prev_action_tag = $default_action_tag;
         $.each(texts, function(tag_name, descr) {
             // Creating a new action tag row.
             var $new_action_tag = $default_action_tag.clone();
-            var $button = $new_action_tag.find('td button');
+            var $cols = $new_action_tag.children('td');
+            var $button = $cols.find('button');
 
             // Column 1: updating button behavior.
             $button.attr('onclick', $button.attr('onclick').replace('@DEFAULT', tag_name));
 
             // Columns 2: updating action tag label.
-            $new_action_tag.children('td').filter(isDefaultLabelColumn).text(tag_name);
+            $cols.filter(isDefaultLabelColumn).text(tag_name);
 
             // Column 3: updating action tag description.
-            $new_action_tag.children('td').last().text(descr);
+            $cols.last().text(descr);
 
             // Placing new action tag.
-            $new_action_tag.insertAfter($prev_action_tag);
-            $prev_action_tag = $new_action_tag;
+            $new_action_tag.insertAfter($default_action_tag);
         });
     });
 });
